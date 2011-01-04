44716207e87b59227d2a247bd1a7ab598e1a3485
Firerat <firer4t@googlemail.com>
2011-01-04 12:15:05 +0000

    Correct file permissions for boot scripts 0700 might be ott, but I see no reason why boot scripts should be world executable
    Change-Id: Ibe4c76d2395ffda711fdf32242212d0f05a6e954

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -157,6 +157,7 @@ static struct fs_path_config android_dirs[] = {
 ** and will allow partial matches.
 */
 static struct fs_path_config android_files[] = {
+    { 00700, AID_ROOT,      AID_ROOT,     "system/etc/init.d/*" },
     { 00440, AID_ROOT,      AID_SHELL,     "system/etc/init.goldfish.rc" },
     { 00550, AID_ROOT,      AID_SHELL,     "system/etc/init.goldfish.sh" },
     { 00440, AID_ROOT,      AID_SHELL,     "system/etc/init.trout.rc" },
@@ -191,6 +192,8 @@ static struct fs_path_config android_files[] = {
 		/* the following file is INTENTIONALLY set-uid, and IS included
 		 * in user builds. */
     { 06750, AID_ROOT,      AID_SHELL,     "system/bin/run-as" },
+        /* the following file should only be executable by root */
+    { 00700, AID_ROOT,      AID_ROOT,      "system/bin/sysinit.rc" },
     { 00755, AID_ROOT,      AID_SHELL,     "system/bin/*" },
     { 00755, AID_ROOT,      AID_SHELL,     "system/xbin/*" },
     { 00755, AID_ROOT,      AID_SHELL,     "system/vendor/bin/*" },
